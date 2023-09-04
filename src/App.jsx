41dd036269b88b7f149c7dc335bb1db56c3ab4cf
jmadupalli <jayanth.madupalli@gmail.com>
2023-09-04 16:41:11 -0500

    added react router, base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { Navbar } from './components';
+
 function App() {
 
   return (
-    <>
-      CoinRanking API from RapidAPI
-    </>
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+
+      </div>
+      <div className='footer'>
+
+      </div>
+    </div>
   )
 }
 
